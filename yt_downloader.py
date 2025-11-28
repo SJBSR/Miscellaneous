95252e48ac5dd384f13b1b8b67bcac9025194d23
SJBSR <sjb.sr@outlook.com>
2025-11-28 13:33:31 -0500

    yt_downloader

---
+++ yt_downloader.py
@@ -0,0 +1,15 @@
+python3 -m pip install pytube
+from pytube import YouTube
+from sys import argv
+
+link = argv[1]
+yt = YouTube(link)
+
+print("Title:", yt.title)
+
+print("View:", yt.views)
+
+yd = yt.streams.get_highest_resolution()
+
+yd.download('/Users/sjbsr/Desktop/MINE_OLD')
+
