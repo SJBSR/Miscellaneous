a7f91bcdad9457ee6c8a6ec1c1004863e151b007
SJBSR <sjb.sr@outlook.com>
2025-11-29 12:14:11 -0500

    pytube_updated

--- yt_downloader.py
+++ yt_downloader.py
@@ -1,14 +1,14 @@
-from pytube import YouTube
+from yt_dlp import YoutubeDL
 from sys import argv
 
 link = argv[1]
-yt = YouTube(link)
+yt = YoutubeDL(link)
 
 print("Title:", yt.title)
 
 print("View:", yt.views)
 
-yd = yt.streams.get_highest_resolution()
+yt = yt.streams.get_highest_resolution()
 
-yd.download('/Users/sjbsr/Desktop/MINE_OLD')
+yt.download('/Users/sjbsr/Desktop/MINE_OLD')
 
